eafa519a8e3810dc558495335296f0c44616612b
romdk <drack.roman67@gmail.com>
2022-10-24 13:44:43 +0200

    ajout fonction aff reservations client

--- classes/Client.class.php
+++ classes/Client.class.php
@@ -32,6 +32,21 @@
         }
         
         public function afficherReservationsClient(){
+            $result = "Réservations de ".$this->_prenom." ".$this->_nom."<br>".count($this->_reservations)." RESERVATIONS<br>";
+            $total = 0;
+            foreach ($this-> _reservations as $reservation){
+                $result .= "Hotel : "
+                .$reservation->getHotel()->getNomHotel()." "
+                .str_repeat("*", ($reservation->getHotel()->getNbEtoile()))." "
+                .$reservation->getHotel()->getVille()." / Chambre : "
+                .$reservation->getChambre()->getNumero()." ("
+                .$reservation->getChambre()->getNbLit()." - "
+                .$reservation->getChambre()->getPrix()."&euro; - Wifi: "
+                .$reservation->getChambre()->getWifi().") "
+                .$reservation->getPeriode()."<br>";
+                $total += $reservation->getChambre()->getPrix();
+            }
+            return $result."Total: ".$total."&euro;";
+            }
                     
-        }
     }
